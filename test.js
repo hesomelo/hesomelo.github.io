9210678a9bd9e881ccba21ae5ed84efafbd2af1a
Oscar Lu <oscarr_luu@hotmail.com>
2023-08-15 13:40:43 -0700

    home button and null check

--- test.js
+++ test.js
@@ -15,12 +15,39 @@ let b = (c) => {
     }
     console.log(c);
 }
+
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("showTextButton").addEventListener("click", function() {
-        b(a);
-        a = !a;
-    });
+
+    let exist = document.getElementById("showTextButton");
+    if(exist){
+        exist.addEventListener("click", function(){
+            b(a);
+            a=!a;
+        });
+    }
+    else{
+        console.log("no button");
+    }
 });
+
+
+
+let go_back = ()=>{
+
+        console.log("click")
+        window.location = ('index.html') 
+        // window.location.href = ('https://careerkarma.com')
+        console.log(window.location)
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    let exist = document.getElementById("return()_but")
+    if(exist){
+        exist.addEventListener("click", function(){
+            go_back();
+        })
+    }
+})
 
 
 
